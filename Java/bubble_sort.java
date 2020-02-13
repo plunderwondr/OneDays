84861ce9fda392bbab933ae5f407c5525d46cffa
plunderwondr <plunderwondr@gmail.com>
2020-02-12 20:45:08 -0500

    new java interview questions

---
+++ Java/bubble_sort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class bubble_sort{
+	public static void main(String[] args)
+	{
+	
+		int [] numbersToSort = new int[] {10,-2,0,14,43,25,18,1,5,45,14};
+		int noSwap = 0;
+		System.out.println(Arrays.toString(numbersToSort));
+		do {
+			noSwap = 0;
+			for(int i = 0; i<(numbersToSort.length - 1); i++)
+			{
+					if(numbersToSort[i] >numbersToSort[i+1]) {
+						//Swaps the two array values without using a temporary value. For clean readable code this should use temporary value.
+						numbersToSort[i] ^= numbersToSort[i+1];
+						numbersToSort[i+1] ^= numbersToSort[i];
+						numbersToSort[i] ^= numbersToSort[i+1];
+					}
+					else 
+					{
+						noSwap++;
+					}
+			}
+		}while(noSwap !=(numbersToSort.length - 1));
+
+		System.out.println(Arrays.toString(numbersToSort));
+		
+	}
+}
---
+++ Java/swap_variables.java
@@ -0,0 +1,33 @@
+// Swamp two variables values without using a tertiary variable.
+
+public class swap_variables{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 20;
+		//using arithmetic
+//		System.out.println(a);
+//		System.out.println(b);
+//		b = a + b;
+//		System.out.println(b);
+//		a= b - a;
+//		System.out.println(a);
+//		b = b - a;
+//		System.out.println(b);
+		
+		//harder to read condensed
+//		a+=b;
+//		a=b-a;
+//		b-=a;
+//		
+		//condensed bitwise
+		b^=a;
+		a^=b;
+
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
+	
